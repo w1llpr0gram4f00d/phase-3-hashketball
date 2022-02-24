a6afa384bc19b5c46534a54fd98f2e2dee1669d5
Marc Cavazos <marcavazos19@icloud.com>
2022-02-23 19:37:09 -0500

    commit tree 997dc23ed70577cfd6317956d3b2edf04fbf6d0a

--- hashketball.rb
+++ hashketball.rb
@@ -1,3 +1,4 @@
+require 'pry'
 # Write your code below game_hash
 def game_hash
   {
@@ -126,4 +127,59 @@ def game_hash
   }
 end
 
+def all_players
+  game_hash[:home][:players] + game_hash[:away][:players]
+end
+
+def find_team team_name
+  team_info = game_hash.find do |location, team_data|
+    team_data[:team_name] == team_name
+  end
+
+  team_info[1]
+end
+
+def player_stats player_name
+  all_players.find do |player|
+    player[:player_name] == player_name
+  end
+end
+
 # Write code here
+def num_points_scored player_name
+  player = player_stats(player_name)
+  player[:points]
+end
+
+def shoe_size player_name
+  player = player_stats(player_name)
+  player[:shoe]
+end
+
+def team_colors team_name
+  team = find_team(team_name)
+  team[:colors]
+end
+
+def team_names
+  game_hash.map do |location, team_data|
+    team_data[:team_name]
+  end
+end
+
+def player_numbers team_name
+  team = find_team(team_name)
+  team[:players].map do |player|
+    player[:number]
+  end
+end
+
+def big_shoe_player
+  all_players.max_by do |player|
+    player[:shoe]
+  end
+end
+
+def big_shoe_rebounds
+  big_shoe_player[:rebounds]
+end
